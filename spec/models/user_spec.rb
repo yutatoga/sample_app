36c65be35cc718834a458a3cc23b42518459866e
yutatoga <yuta.toga@gmail.com>
2013-08-30 22:35:14 +0900

    around @user.valid?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,8 @@ describe User do
 	subject{@user}
 	it{should respond_to(:name)}
 	it{should respond_to(:email)}
+	it{should be_valid}
+	describe "when name is not present" do
+		before{@user.name = " "}
+		it {should_not be_valid}
 end
