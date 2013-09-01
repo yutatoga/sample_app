66a2faa29b4e912ba379cdb56708fe495aeb136a
yutatoga <yuta.toga@gmail.com>
2013-09-02 00:38:30 +0900

    done 6.3.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe User do
-	before{@user = User.new(name: "Example User", email: "user@example.com")}
+	before{@user = User.new(name: "Example User", email: "user@example.com", password:"foobar", password_confirmation:"foobar")}
 	subject{@user}
 	it{should respond_to(:name)}
 	it{should respond_to(:email)}
+	it{should respond_to(:password_digest)}
+	it{should respond_to(:password)}
+	it{should respond_to(:password_confirmation)}
+	it{should respond_to(:authenticate)}
+
 	it{should be_valid}
 	describe "when name is not present" do
 		before{@user.name = " "}
@@ -24,7 +29,7 @@ describe User do
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
-			end
+			end;
 		end
 	end
 	describe "when email format is valid" do
@@ -50,5 +55,31 @@ describe User do
 			user_with_same_email.save
 		end
 		it{should_not be_valid}
+	end
+	describe "when password is not present" do
+		before do
+			@user = User.new(name: "Example User", email:"user@example.com", password:" ", password_confirmation: " ")
+		end
+		it{should_not be_valid}
+	end
+	describe "when password doesn't match confirmation" do
+		before {@user.password_confirmation = "mismatch"}
+		it{should_not be_valid}
+	end
+	describe "return value of authenticate" do
+		before{@user.save}
+		let(:found_user) {User.find_by(email: @user.email)}
+		describe "with valid password" do
+			it{should eq found_user.authenticate(@user.password)}
+		end
+		describe "with invalid password" do
+			let(:user_for_invalid_password){found_user.authenticate("invalid")}
+			it{should_not eq user_for_invalid_password}
+			specify{expect(user_for_invalid_password).to be_false}
+		end
+	end
+	describe "with a password that's too short" do
+		before {@user.password = @user.password_confirmation = "a"*5}
+		it{should be_invalid}
 	end
 end
